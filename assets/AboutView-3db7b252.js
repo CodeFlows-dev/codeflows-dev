41046169a3f1c2d126b1749d6d952d941cd40664
dxps <marius.ileana@gmail.com>
2023-07-17 10:58:59 +0300

    Updates

--- assets/AboutView-3db7b252.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as o,a as s}from"./index-c2e46f3e.js";import"logo.png";const _={},c={class:"about"},a=s("h1",null,"This is an about page",-1),n=[a];function i(r,u){return t(),o("div",c,n)}const p=e(_,[["render",i]]);export{p as default};
