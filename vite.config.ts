a775378f47f4614ad28e142871e1a97a088d6130
dxps <marius.ileana@gmail.com>
2023-07-17 12:12:11 +0300

    fix: logo.png path, dist upd

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['/logo.png']
+      external: ['logo.png']
     }
   },
   plugins: [vue()],
